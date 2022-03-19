7549e7456586a725b287530c3e5b31b965811960
ShubhamPatil15 <pshubhamc44@gmail.com>
2022-03-19 07:31:26 -0700

    MODIFIED: Employee Type

--- src/com/rcpit/EmplyeeWage.java
+++ src/com/rcpit/EmplyeeWage.java
@@ -10,6 +10,12 @@ public class EmplyeeWage {
             System.out.println("Employee is Absent");
         }else{
             System.out.println("Employee is present");
+            int b = random.nextInt() % 2;
+            if(b == 0){
+                System.out.println("Employee is Part time");
+            }else{
+                System.out.println("Employee is Full time");
+            }
         }
     }
 }
